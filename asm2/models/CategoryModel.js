e75ad071be0f2d998264bebcb70d94a0714d803e
Hoangzxczxc <139453473+Hoangzxczxc@users.noreply.github.com>
2023-12-16 01:17:54 +0700

    update model

---
+++ asm2/models/CategoryModel.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var CategorySchema = mongoose.Schema({
+    name: String
+});
+
+var CategoryModel = mongoose.model('categories', CategorySchema);
+module.exports = CategoryModel;
