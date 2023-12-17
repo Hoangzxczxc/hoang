1223629f4e0317dd918acc8d063a81257f6fb50c
Hoangzxczxc <139453473+Hoangzxczxc@users.noreply.github.com>
2023-12-17 16:04:12 +0700

    delete

--- asm2/models/ManufacturerModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var ManufacturerSchema = mongoose.Schema({
-    name: String,
-    year: String,
-    image: String,
-});
-
-var ManufacturerModel = mongoose.model('manufacturers', ManufacturerSchema);
-module.exports = ManufacturerModel;
